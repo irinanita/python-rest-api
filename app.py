2fca87623a1cc30886dcabb007a8b532746a5bb0
Irina Nita <irinanita.im@gmail.com>
2019-10-01 23:40:59 +0100

    Refined comments. Added json object in media info that is returned at the request.

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
+from flask import jsonify
 import requests
 import platform
-from bs4 import BeautifulSoup
 import json
+from bs4 import BeautifulSoup
 from PIL import Image
 from io import BytesIO
 import sys
-
 
 
 app = Flask(__name__)
@@ -14,60 +14,81 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World"
+    return "This is an API in Python"
 
 @app.route('/ping')
 def pong():
-    return "Pong"
+    return "pong"
 
 @app.route('/system')
 def system():
-    system_info_dict = {"OS": platform.system(),
+    system_info_dict = {"Python API": "v1",
+                   "OS": platform.system(),
                    "Processor": platform.processor(),
                    "Python Version": platform.python_version()}
-    system_info_json = json.dumps(system_info_dict)
+    return jsonify(system_info_dict)
 
-    return system_info_json
 
 @app.route('/mediainfo/<string:id>')
 def mediainfo(id):
-    try:
-        response = requests.get('https://www.pond5.com/photo/' + id + '/') #retrieve the web page with our data
-        '''
-        We create a new BeautifulSoup object by passing 
-        the constructor our newly acquired HTML content 
-        and the type of parser we want to use:
-        '''
-        soup = BeautifulSoup(response.content, 'html.parser') # parse the retrieved response
-        #Retrieve image title
-        our_image = soup.select_one('div[item_id="' + id + '"]')['formats_data']
-        image_data = json.loads(our_image)
-        print(image_data)
-        print(image_data['title'])
+        try:
+            '''
+            Retrieve the web page of the image with the specified id
+            '''
 
-        #Retrieve image url
-        url = soup.select_one('div[item_id="' + id + '"] img')['src']
-        image_filename = url.split('/')[-1]
-        print(url)
-        print(image_filename)
+            response = requests.get('https://www.pond5.com/photo/' + id + '/')
 
-        data = requests.get(url).content
-        '''
-        Load image  
-        '''
-        im = Image.open(BytesIO(data))
+            '''
+            Create a new BeautifulSoup object by passing 
+            the constructor our acquired HTML content 
+            and the type of parser we want to use :
+            '''
 
-        width, height = im.size
+            soup = BeautifulSoup(response.content, 'html.parser')
 
-        print("img size in memory in bytes: ", sys.getsizeof(im.tobytes()))
-        print(width, height)
+            '''
+            Retrieve image title from the div element
+            with the specific item_id that matches the
+            <id> in the request
+            '''
+
+            image_formats_data_attirbute = soup.select_one('div[item_id="' + id + '"]')['formats_data']
+
+            '''
+            Retrieve the value of the attribute
+            parse it to json and
+            get the value corresponding to the key "title"
+            '''
+            formats_data_attirbute_json = json.loads(image_formats_data_attirbute )
+
+            image_title = formats_data_attirbute_json['title']
+
+            '''
+            Retrieve image url from the img tag source attribute
+            Split the link and retrieve the last element that reffers 
+            to the filename
+            '''
+            url = soup.select_one('div[item_id="' + id + '"] img')['src']
+            image_filename = url.split('/')[-1]
+
+            '''
+            Load image from url retrieved above
+            and get size  
+            '''
+            data = requests.get(url).content
+            im = Image.open(BytesIO(data))
+            width, height = im.size
+            size_bytes = sys.getsizeof(im.tobytes())
+
+            image_info = {"Title": image_title,
+                          "Filename": image_filename,
+                          "Width": width,
+                          "Height": height,
+                          "Size(B)": size_bytes}
+
+            return jsonify(image_info)
+        except:
+            return "An error occurred, please check if the url is correct."
 
 
-        # Retrieve image title option 2 from DOM
-        #image_title = soup.select_one('#itemDetail-mediaTitle span').text
-        #print(image_title)
-
-        return "JSON object with image filename, size, dimensions and image title"
-    except:
-        return "Something went wrong. Please check if the the endpoint is correct"
-app.run(port=5000, debug=True)
+app.run(port=5000, debug=False)
